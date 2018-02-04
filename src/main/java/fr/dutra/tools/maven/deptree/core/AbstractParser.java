5c3f181e082271fdb203b024055f45681e0e98af
ChenLuigi <chen.luigi@whitesourcesoftware.com>
2018-02-04 14:05:30 +0200

    WSE-205 Fix the parser when trying to parse description of dependency

--- src/main/java/fr/dutra/tools/maven/deptree/core/AbstractParser.java
+++ src/main/java/fr/dutra/tools/maven/deptree/core/AbstractParser.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2011 Alexandre Dutra
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package fr.dutra.tools.maven.deptree.core;
 
@@ -39,9 +39,9 @@ public abstract class AbstractParser implements Parser {
         int tokenEnd = 0;
         for (; tokenEnd < artifact.length(); tokenEnd++) {
             final char c = artifact.charAt(tokenEnd);
-            switch (c){
+            switch (c) {
                 case ' ': // in descriptions only
-                    if(tokenStarted && ! hasDescription) {
+                    if (tokenStarted && !hasDescription) {
                         tokens.add(artifact.substring(tokenStart, tokenEnd));
                         tokenStarted = false;
                         hasDescription = true;
@@ -49,22 +49,28 @@ public abstract class AbstractParser implements Parser {
                     continue;
 
                 case ':':
-                case ')': //end of descriptions and omitted artifacts
                     tokens.add(artifact.substring(tokenStart, tokenEnd));
                     tokenStarted = false;
+                    continue;
+                case ')': //end of descriptions and omitted artifacts,
+                    // check length in case of "com.bugsnag:bugsnag:jar:3.1.4:compile (version selected from constraint [3.0,4.0))"
+                    if (tokenEnd == artifact.length() - 1) {
+                        tokens.add(artifact.substring(tokenStart, tokenEnd));
+                        tokenStarted = false;
+                    }
                     continue;
 
                 case '-': // in omitted artifacts descriptions
                     continue;
 
                 case '(': // in omitted artifacts
-                    if(tokenEnd == 0) {
+                    if (tokenEnd == 0) {
                         omitted = true;
                     }
                     continue;
 
                 default:
-                    if(! tokenStarted) {
+                    if (!tokenStarted) {
                         tokenStart = tokenEnd;
                         tokenStarted = true;
                     }
@@ -73,7 +79,7 @@ public abstract class AbstractParser implements Parser {
         }
 
         //last token
-        if(tokenStarted) {
+        if (tokenStarted) {
             tokens.add(artifact.substring(tokenStart, tokenEnd));
         }
 
@@ -85,7 +91,7 @@ public abstract class AbstractParser implements Parser {
         String scope;
         String description;
 
-        if(tokens.size() == 4) {
+        if (tokens.size() == 4) {
 
             groupId = tokens.get(0);
             artifactId = tokens.get(1);
@@ -95,7 +101,7 @@ public abstract class AbstractParser implements Parser {
             description = null;
             classifier = null;
 
-        } else if(tokens.size() == 5) {
+        } else if (tokens.size() == 5) {
 
             groupId = tokens.get(0);
             artifactId = tokens.get(1);
@@ -105,9 +111,9 @@ public abstract class AbstractParser implements Parser {
             description = null;
             classifier = null;
 
-        } else if(tokens.size() == 6) {
+        } else if (tokens.size() == 6) {
 
-            if(hasDescription) {
+            if (hasDescription) {
                 groupId = tokens.get(0);
                 artifactId = tokens.get(1);
                 packaging = tokens.get(2);
@@ -125,7 +131,7 @@ public abstract class AbstractParser implements Parser {
                 description = null;
             }
 
-        } else if(tokens.size() == 7) {
+        } else if (tokens.size() == 7) {
 
             groupId = tokens.get(0);
             artifactId = tokens.get(1);
@@ -140,14 +146,14 @@ public abstract class AbstractParser implements Parser {
         }
 
         final Node node = new Node(
-            groupId,
-            artifactId,
-            packaging,
-            classifier,
-            version,
-            scope,
-            description,
-            omitted
+                groupId,
+                artifactId,
+                packaging,
+                classifier,
+                version,
+                scope,
+                description,
+                omitted
         );
         return node;
     }
